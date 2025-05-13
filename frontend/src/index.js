aa49a104b166cb525acedc53655a535782a2a4b4
JavierABADdelMolino <152556679+JavierABADdelMolino@users.noreply.github.com>
2025-05-14 00:41:57 +0200

    ✨ feat(theme): incorporación de temas, personalizando boostrap. Los temas son solo provisionales

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,18 +9,21 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { LoaderProvider } from './context/LoaderContext';
 import Loader from './components/Loader';
+import { ThemeProvider } from './context/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <LoaderProvider>
-      <AuthProvider>
-        <BrowserRouter>
-          <Loader />
-          <RouteChangeLoader /> {/* 👈 Loader al navegar entre páginas */}
-          <App />
-        </BrowserRouter>
-      </AuthProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <BrowserRouter>
+            <Loader />
+            <RouteChangeLoader /> {/* 👈 Loader al navegar entre páginas */}
+            <App />
+          </BrowserRouter>
+        </AuthProvider>
+      </ThemeProvider>
     </LoaderProvider>
   </React.StrictMode>
 );
